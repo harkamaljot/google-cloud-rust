4734b8f106c8b2a0370840f43313043e868d7b0a
Harkamal Jot Singh Kumar <harkamalj@google.com>
2024-12-27 09:07:40 +0000

    fetch mds token

--- src/auth/src/credentials/mds_credential.rs
+++ src/auth/src/credentials/mds_credential.rs
@@ -124,12 +124,9 @@ impl MDSAccessTokenProvider {
 impl TokenProvider for MDSAccessTokenProvider {
     async fn get_token(&mut self) -> Result<Token> {
         let request = Client::new();
-        let service_account = MDSAccessTokenProvider::get_service_account_info(&request, self.token_endpoint.clone(), Option::None).await?;
-        let mut params = HashMap::new();
-        if service_account.scopes.is_some() {
-            params.insert("scopes", service_account.scopes.unwrap().join(","));
-        }
-        let path = format!("instance/service-accounts/{}/token", service_account.email);
+        let service_account = "default";
+        let params: HashMap<String, String> = HashMap::new();
+        let path = format!("instance/service-accounts/{}/token", service_account);
         let mut headers = HeaderMap::new();
         headers.insert(
             METADATA_FLAVOR,
